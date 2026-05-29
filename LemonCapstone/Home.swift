426f623b2b84ad3e795575c3e6baf0b84f7988b2
Geovani Mendoza <geovanimendoza.h@gmail.com>
2026-05-29 19:03:54 -0400

    feat: implement fetching and storing the food menu

--- LemonCapstone/Home.swift
+++ LemonCapstone/Home.swift
@@ -1,17 +1,24 @@
 import SwiftUI
+import CoreData
 
 struct Home: View {
+    let persistence = PersistenceController.shared
+    @State private var selectedTab = 0
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             Menu()
+                .environment(\.managedObjectContext, persistence.container.viewContext)
                 .tabItem {
                     Label("Menu", systemImage: "list.dash")
                 }
+                .tag(0)
             
-            UserProfile()
+            UserProfile(selectedTab: $selectedTab)
                 .tabItem {
                     Label("Profile", systemImage: "square.and.pencil")
                 }
+                .tag(1)
         }
         .navigationBarBackButtonHidden(true)
     }
---
+++ LemonCapstone/MenuItem.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct MenuItem: Decodable {
+    let title: String
+    let image: String
+    let price: String
+    let description: String
+    let category: String
+}
