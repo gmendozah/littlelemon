ed6936e364266974c75cb2280205458eb817b899
Geovani Mendoza <geovanimendoza.h@gmail.com>
2026-05-29 13:25:01 -0400

    Initial Commit

---
+++ LemonCapstone/LemonCapstoneApp.swift
@@ -0,0 +1,21 @@
+//
+//  LemonCapstoneApp.swift
+//  LemonCapstone
+//
+//  Created by Geovani Mendoza on 29/5/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct LemonCapstoneApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ LemonCapstoneTests/LemonCapstoneTests.swift
@@ -0,0 +1,19 @@
+//
+//  LemonCapstoneTests.swift
+//  LemonCapstoneTests
+//
+//  Created by Geovani Mendoza on 29/5/26.
+//
+
+import Testing
+@testable import LemonCapstone
+
+struct LemonCapstoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
